e7b4c074e41270152c348781bc3b165d796c4ab0
LiteLT <40676441+LiteLT@users.noreply.github.com>
2020-09-04 12:48:23 -0400

    Initial Commit

---
+++ Shared/Anime_CatalogApp.swift
@@ -0,0 +1,17 @@
+//
+//  Anime_CatalogApp.swift
+//  Shared
+//
+//  Created by Kyle Erhabor on 9/4/20.
+//
+
+import SwiftUI
+
+@main
+struct Anime_CatalogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
