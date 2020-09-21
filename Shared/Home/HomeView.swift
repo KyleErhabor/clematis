fe764b1de633b169beffe3d7fe680fd2b002008a
LiteLT <40676441+LiteLT@users.noreply.github.com>
2020-09-21 16:17:48 -0400

    feat: add activity feed views

--- Shared/Home/HomeView.swift
+++ Shared/Home/HomeView.swift
@@ -6,15 +6,92 @@
 //
 
 import SwiftUI
+import SDWebImageSwiftUI
 
 struct HomeView: View {
     @StateObject private var viewModel = HomeViewModel()
 
     var body: some View {
-        List(viewModel.activities) { activity in
-            Text("ID: \(activity.id)")
+        NavigationView {
+            List {
+                ForEach(viewModel.activities) { activity in
+                    if let activity = activity.listActivityFragment {
+                        HomeListActivityView(activity: activity)
+                    }
+                }
+            }.navigationBarTitle("Activity Feed")
         }.onAppear {
             viewModel.loadActivities()
+        }
+    }
+}
+
+fileprivate struct HomeListActivityView: View {
+    fileprivate let activity: ListActivityFragment
+
+    var body: some View {
+        NavigationLink(destination: MediaView()) {
+            let coverImageURL = activity.media?.coverImage?.extraLarge == nil
+                ? nil
+                : URL(string: (activity.media?.coverImage?.extraLarge)!)
+
+            let coverImageColor = activity.media?.coverImage?.color == nil
+                ? Color.primary
+                : Color.init(hex: Int((activity.media?.coverImage?.color?.dropFirst())!, radix: 16)!)
+
+            WebImage(url: coverImageURL)
+                .resizable()
+                .background(coverImageColor)
+                .scaledToFit()
+                .cornerRadius(5)
+                .frame(width: 80, height: 128)
+
+
+            HomeListActivitySummaryView(activity: activity).padding(5)
+        }.animation(.easeIn(duration: 0.3))
+    }
+}
+
+fileprivate struct HomeListActivitySummaryView: View {
+    fileprivate let activity: ListActivityFragment
+
+    private var dateFormatter: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.timeStyle = .short
+        formatter.dateStyle = .short
+
+        return formatter
+    }
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            HStack {
+                Text(activity.user?.name ?? "?").bold()
+                Spacer()
+                Text(dateFormatter.string(from: Date(timeIntervalSince1970: TimeInterval(activity.createdAt))))
+                    .foregroundColor(.gray)
+                    .font(.footnote)
+            }
+
+            let status = activity.status?.capitalizeFirstLetter() ?? "?"
+            let title = activity.media?.title?.userPreferred ?? "?"
+
+            if activity.progress == nil {
+                // Completed <x>
+                Text("\(status) ") + Text("\(title)").bold()
+            } else {
+                // Watched episode (<x> - <y> | <x>) of <z>
+                Text("\(status) \(activity.progress!) of ") + Text("\(title)").bold()
+            }
+
+            if let avatar = activity.user?.avatar?.large,
+               let avatarURL = URL(string: avatar) {
+                WebImage(url: avatarURL)
+                    .resizable()
+                    .scaledToFit()
+                    .cornerRadius(5)
+                    .frame(width: 40, height: 40)
+            }
         }
     }
 }
--- Shared/Home/HomeViewModel.swift
+++ Shared/Home/HomeViewModel.swift
@@ -9,14 +9,21 @@ import Foundation
 import Apollo
 
 class HomeViewModel: ObservableObject {
-    @Published private(set) var activities = [GlobalActivityFeedQuery.Data.Page.Activity.Fragments]()
+    @Published private(set) var activities = [ActivityFeedQuery.Data.Page.Activity.Fragments]()
 
     func loadActivities() {
-        ApolloNetwork.shared.anilist.fetch(query: GlobalActivityFeedQuery(page: 1, perPage: 50)) { result in
+        ApolloNetwork.shared.anilist.fetch(query: ActivityFeedQuery(page: 1, perPage: 50)) { result in
             switch result {
                 case .success(let queryData):
                     if let activities = queryData.data?.page?.activities {
-                        self.activities.append(contentsOf: activities.compactMap { $0?.fragments })
+                        self.activities.append(contentsOf: activities.compactMap {
+                            if let isAdult = $0?.fragments.listActivityFragment?.media?.isAdult,
+                               isAdult == true {
+                                return nil
+                            }
+
+                            return $0?.fragments
+                        })
                     }
                 case .failure(let err): print(err) // TODO: Handle the error.
             }
