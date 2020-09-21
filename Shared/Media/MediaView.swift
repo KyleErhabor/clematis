da93d857a6676e96960322a11823f582b0d114af
LiteLT <40676441+LiteLT@users.noreply.github.com>
2020-09-21 16:17:26 -0400

    chore: add template file

---
+++ Shared/Media/MediaView.swift
@@ -0,0 +1,20 @@
+//
+//  MediaView.swift
+//  Anime Catalog
+//
+//  Created by Kyle Erhabor on 9/20/20.
+//
+
+import SwiftUI
+
+struct MediaView: View {
+    var body: some View {
+        Text("Hello, World (media)!")
+    }
+}
+
+struct MediaView_Previews: PreviewProvider {
+    static var previews: some View {
+        MediaView()
+    }
+}
