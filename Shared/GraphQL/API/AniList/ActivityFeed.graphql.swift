a62cc1cb90429d2bf229f3e400f5aff910411df9
LiteLT <kyleerhabor@gmail.com>
2020-11-22 09:25:19 -0500

    Add ActivityFeed & remove UserActivity GraphQL queries
    `UserView` will be removed in a later commit, so this GraphQL query is being removed in the meantime.
    At the time, there plans to be two queries related to activity: the Global/Following feed, and a user feed.
    The two queries will be called `ActivityFeed` and `UserActivity`. There was plan to have one GraphQL query
    to do both Following/Global & user activity, but the AniList API returns an empty array when `Page.activities`'s
    `userId` argument is `null`. This seems to be an issue, considering it does not make sense to not throw an error,
    or factor in the possibility the requester meant any user. A GitHub issue has been opened to address this issue:
    https://github.com/AniList/ApiV2-GraphQL-Docs/issues/106
    
    Downloading the repository from this commit will likely result is compilation errors. This should only be used as
    a reference to previous changes.

---
+++ Shared/GraphQL/API/AniList/ActivityFeed.graphql.swift
@@ -0,0 +1,310 @@
+// @generated
+//  This file was automatically generated and should not be edited.
+
+import Apollo
+import Foundation
+
+public final class ActivityFeedQuery: GraphQLQuery {
+  /// The raw GraphQL definition of this operation.
+  public let operationDefinition: String =
+    """
+    query ActivityFeed($page: Int!, $perPage: Int!, $isFollowing: Boolean!, $hasRepliesOrTypeText: Boolean!) {
+      Page(page: $page, perPage: $perPage) {
+        __typename
+        activities(isFollowing: $isFollowing, hasRepliesOrTypeText: $hasRepliesOrTypeText, sort: [ID_DESC]) {
+          __typename
+          ... on TextActivity {
+            id
+          }
+          ... on ListActivity {
+            id
+          }
+          ... on MessageActivity {
+            id
+          }
+        }
+      }
+    }
+    """
+
+  public let operationName: String = "ActivityFeed"
+
+  public var page: Int
+  public var perPage: Int
+  public var isFollowing: Bool
+  public var hasRepliesOrTypeText: Bool
+
+  public init(page: Int, perPage: Int, isFollowing: Bool, hasRepliesOrTypeText: Bool) {
+    self.page = page
+    self.perPage = perPage
+    self.isFollowing = isFollowing
+    self.hasRepliesOrTypeText = hasRepliesOrTypeText
+  }
+
+  public var variables: GraphQLMap? {
+    return ["page": page, "perPage": perPage, "isFollowing": isFollowing, "hasRepliesOrTypeText": hasRepliesOrTypeText]
+  }
+
+  public struct Data: GraphQLSelectionSet {
+    public static let possibleTypes: [String] = ["Query"]
+
+    public static var selections: [GraphQLSelection] {
+      return [
+        GraphQLField("Page", arguments: ["page": GraphQLVariable("page"), "perPage": GraphQLVariable("perPage")], type: .object(Page.selections)),
+      ]
+    }
+
+    public private(set) var resultMap: ResultMap
+
+    public init(unsafeResultMap: ResultMap) {
+      self.resultMap = unsafeResultMap
+    }
+
+    public init(page: Page? = nil) {
+      self.init(unsafeResultMap: ["__typename": "Query", "Page": page.flatMap { (value: Page) -> ResultMap in value.resultMap }])
+    }
+
+    public var page: Page? {
+      get {
+        return (resultMap["Page"] as? ResultMap).flatMap { Page(unsafeResultMap: $0) }
+      }
+      set {
+        resultMap.updateValue(newValue?.resultMap, forKey: "Page")
+      }
+    }
+
+    public struct Page: GraphQLSelectionSet {
+      public static let possibleTypes: [String] = ["Page"]
+
+      public static var selections: [GraphQLSelection] {
+        return [
+          GraphQLField("__typename", type: .nonNull(.scalar(String.self))),
+          GraphQLField("activities", arguments: ["isFollowing": GraphQLVariable("isFollowing"), "hasRepliesOrTypeText": GraphQLVariable("hasRepliesOrTypeText"), "sort": ["ID_DESC"]], type: .list(.object(Activity.selections))),
+        ]
+      }
+
+      public private(set) var resultMap: ResultMap
+
+      public init(unsafeResultMap: ResultMap) {
+        self.resultMap = unsafeResultMap
+      }
+
+      public init(activities: [Activity?]? = nil) {
+        self.init(unsafeResultMap: ["__typename": "Page", "activities": activities.flatMap { (value: [Activity?]) -> [ResultMap?] in value.map { (value: Activity?) -> ResultMap? in value.flatMap { (value: Activity) -> ResultMap in value.resultMap } } }])
+      }
+
+      public var __typename: String {
+        get {
+          return resultMap["__typename"]! as! String
+        }
+        set {
+          resultMap.updateValue(newValue, forKey: "__typename")
+        }
+      }
+
+      public var activities: [Activity?]? {
+        get {
+          return (resultMap["activities"] as? [ResultMap?]).flatMap { (value: [ResultMap?]) -> [Activity?] in value.map { (value: ResultMap?) -> Activity? in value.flatMap { (value: ResultMap) -> Activity in Activity(unsafeResultMap: value) } } }
+        }
+        set {
+          resultMap.updateValue(newValue.flatMap { (value: [Activity?]) -> [ResultMap?] in value.map { (value: Activity?) -> ResultMap? in value.flatMap { (value: Activity) -> ResultMap in value.resultMap } } }, forKey: "activities")
+        }
+      }
+
+      public struct Activity: GraphQLSelectionSet {
+        public static let possibleTypes: [String] = ["TextActivity", "ListActivity", "MessageActivity"]
+
+        public static var selections: [GraphQLSelection] {
+          return [
+            GraphQLTypeCase(
+              variants: ["TextActivity": AsTextActivity.selections, "ListActivity": AsListActivity.selections, "MessageActivity": AsMessageActivity.selections],
+              default: [
+                GraphQLField("__typename", type: .nonNull(.scalar(String.self))),
+              ]
+            )
+          ]
+        }
+
+        public private(set) var resultMap: ResultMap
+
+        public init(unsafeResultMap: ResultMap) {
+          self.resultMap = unsafeResultMap
+        }
+
+        public static func makeTextActivity(id: Int) -> Activity {
+          return Activity(unsafeResultMap: ["__typename": "TextActivity", "id": id])
+        }
+
+        public static func makeListActivity(id: Int) -> Activity {
+          return Activity(unsafeResultMap: ["__typename": "ListActivity", "id": id])
+        }
+
+        public static func makeMessageActivity(id: Int) -> Activity {
+          return Activity(unsafeResultMap: ["__typename": "MessageActivity", "id": id])
+        }
+
+        public var __typename: String {
+          get {
+            return resultMap["__typename"]! as! String
+          }
+          set {
+            resultMap.updateValue(newValue, forKey: "__typename")
+          }
+        }
+
+        public var asTextActivity: AsTextActivity? {
+          get {
+            if !AsTextActivity.possibleTypes.contains(__typename) { return nil }
+            return AsTextActivity(unsafeResultMap: resultMap)
+          }
+          set {
+            guard let newValue = newValue else { return }
+            resultMap = newValue.resultMap
+          }
+        }
+
+        public struct AsTextActivity: GraphQLSelectionSet {
+          public static let possibleTypes: [String] = ["TextActivity"]
+
+          public static var selections: [GraphQLSelection] {
+            return [
+              GraphQLField("__typename", type: .nonNull(.scalar(String.self))),
+              GraphQLField("id", type: .nonNull(.scalar(Int.self))),
+            ]
+          }
+
+          public private(set) var resultMap: ResultMap
+
+          public init(unsafeResultMap: ResultMap) {
+            self.resultMap = unsafeResultMap
+          }
+
+          public init(id: Int) {
+            self.init(unsafeResultMap: ["__typename": "TextActivity", "id": id])
+          }
+
+          public var __typename: String {
+            get {
+              return resultMap["__typename"]! as! String
+            }
+            set {
+              resultMap.updateValue(newValue, forKey: "__typename")
+            }
+          }
+
+          /// The id of the activity
+          public var id: Int {
+            get {
+              return resultMap["id"]! as! Int
+            }
+            set {
+              resultMap.updateValue(newValue, forKey: "id")
+            }
+          }
+        }
+
+        public var asListActivity: AsListActivity? {
+          get {
+            if !AsListActivity.possibleTypes.contains(__typename) { return nil }
+            return AsListActivity(unsafeResultMap: resultMap)
+          }
+          set {
+            guard let newValue = newValue else { return }
+            resultMap = newValue.resultMap
+          }
+        }
+
+        public struct AsListActivity: GraphQLSelectionSet {
+          public static let possibleTypes: [String] = ["ListActivity"]
+
+          public static var selections: [GraphQLSelection] {
+            return [
+              GraphQLField("__typename", type: .nonNull(.scalar(String.self))),
+              GraphQLField("id", type: .nonNull(.scalar(Int.self))),
+            ]
+          }
+
+          public private(set) var resultMap: ResultMap
+
+          public init(unsafeResultMap: ResultMap) {
+            self.resultMap = unsafeResultMap
+          }
+
+          public init(id: Int) {
+            self.init(unsafeResultMap: ["__typename": "ListActivity", "id": id])
+          }
+
+          public var __typename: String {
+            get {
+              return resultMap["__typename"]! as! String
+            }
+            set {
+              resultMap.updateValue(newValue, forKey: "__typename")
+            }
+          }
+
+          /// The id of the activity
+          public var id: Int {
+            get {
+              return resultMap["id"]! as! Int
+            }
+            set {
+              resultMap.updateValue(newValue, forKey: "id")
+            }
+          }
+        }
+
+        public var asMessageActivity: AsMessageActivity? {
+          get {
+            if !AsMessageActivity.possibleTypes.contains(__typename) { return nil }
+            return AsMessageActivity(unsafeResultMap: resultMap)
+          }
+          set {
+            guard let newValue = newValue else { return }
+            resultMap = newValue.resultMap
+          }
+        }
+
+        public struct AsMessageActivity: GraphQLSelectionSet {
+          public static let possibleTypes: [String] = ["MessageActivity"]
+
+          public static var selections: [GraphQLSelection] {
+            return [
+              GraphQLField("__typename", type: .nonNull(.scalar(String.self))),
+              GraphQLField("id", type: .nonNull(.scalar(Int.self))),
+            ]
+          }
+
+          public private(set) var resultMap: ResultMap
+
+          public init(unsafeResultMap: ResultMap) {
+            self.resultMap = unsafeResultMap
+          }
+
+          public init(id: Int) {
+            self.init(unsafeResultMap: ["__typename": "MessageActivity", "id": id])
+          }
+
+          public var __typename: String {
+            get {
+              return resultMap["__typename"]! as! String
+            }
+            set {
+              resultMap.updateValue(newValue, forKey: "__typename")
+            }
+          }
+
+          /// The id of the activity
+          public var id: Int {
+            get {
+              return resultMap["id"]! as! Int
+            }
+            set {
+              resultMap.updateValue(newValue, forKey: "id")
+            }
+          }
+        }
+      }
+    }
+  }
+}
