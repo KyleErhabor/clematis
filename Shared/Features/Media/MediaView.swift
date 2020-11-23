f42904ccc868e9644567f340b0e200b16bc6eefe
LiteLT <kyleerhabor@gmail.com>
2020-11-23 18:16:00 -0500

    Add User and Media template views

---
+++ Shared/Features/Media/MediaView.swift
@@ -0,0 +1,20 @@
+//
+//  MediaView.swift
+//  Amincapp
+//
+//  Created by Kyle Erhabor on 11/23/20.
+//
+
+import SwiftUI
+
+struct MediaView: View {
+    var body: some View {
+        Text("TODO (media)")
+    }
+}
+
+struct MediaView_Previews: PreviewProvider {
+    static var previews: some View {
+        MediaView()
+    }
+}
---
+++ Shared/Features/User/UserView.swift
@@ -0,0 +1,20 @@
+//
+//  UserView.swift
+//  Amincapp
+//
+//  Created by Kyle Erhabor on 11/23/20.
+//
+
+import SwiftUI
+
+struct UserView: View {
+    var body: some View {
+        Text("TODO (user)")
+    }
+}
+
+struct UserView_Previews: PreviewProvider {
+    static var previews: some View {
+        UserView()
+    }
+}
