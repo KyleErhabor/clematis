ba0b7c74646a4252ce71d924bfec46f41b359288
LiteLT <kyleerhabor@gmail.com>
2020-12-30 18:27:14 -0500

    Add Sidebar view
    The sidebar serves as a replacement to the standard TabView we've been using. This will allow us to house more navigation views to destinations the user probably cares about, but it brings the downside of needing to pull up the sidebar to see it. On an iPad, this is fine since the user can always access the sidebar. On an iPhone, however, unless the screen is in landscape mode, the user must navigate all the way back to the first view to access the sidebar. The user could hold down on the back button to see a list of previous views they were at, but it would be nicer if the sidebar was always accessible.
    
    The second issue with sidebars is how they perform when hidden. ContentView.swift explains the issue with the way SwiftUI handles presenting views.

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -10,8 +10,17 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         NavigationView {
+            // In portrait mode on iOS & iPadOS, the sidebar selects the first view and presents it -- as
+            // expected. In landscape mode, a blank view is the default view. It only presents the first presented
+            // view on the sidebar when the sidebar is clicked.
+            //
+            // The sidebar should automatically select the first view upon loading the app, so this is not
+            // expected behavior in landscape mode. As a workaround, the activity feed view will be shown as the
+            // first view, but this should reasonably be fixed before production. If the user clicks on the
+            // sidebar, it immediately subtracts their click and reloads the activity feed view, which isn't
+            // pleasant.
             SidebarNavigationView()
-
+            
             ActivityFeedView()
         }
     }
--- Shared/Navigation/SidebarNavigationView.swift
+++ Shared/Navigation/SidebarNavigationView.swift
@@ -9,29 +9,68 @@ import SwiftUI
 
 struct SidebarNavigationView: View {
     @EnvironmentObject private var currentUser: CurrentUser
+    @State private var selection: Int? = 0
+    @State private var isPresenting = false
 
     var body: some View {
+        // Refer to the note in `ContentView.swift`
         List {
             Section(header: Text("AniList")) {
-                NavigationLink(destination: ActivityFeedView()) {
+                NavigationLink(destination: ActivityFeedView(), tag: 0, selection: $selection) {
                     Label("Home", systemImage: "house")
                 }
             }
 
             Section(header: Text("Profile")) {
                 if currentUser.users.isEmpty {
-                    NavigationLink(destination: UserView()) {
+                    NavigationLink(destination: UserView(), tag: 1, selection: $selection) {
                         Label("My Profile", systemImage: "person.crop.circle")
                     }
                 } else {
-                    NavigationLink(destination: UserView()) {
+                    NavigationLink(destination: UserView(), tag: 1, selection: $selection) {
                         Label("\(currentUser.users[0].name)", systemImage: "person.crop.circle")
                     }
                 }
             }
+
+            // On macOS, we'll likely hide the Menu controls since the user can access the settings instead.
+            if currentUser.users.isEmpty {
+                Link(destination: AniList.authorizationURL) {
+                    Label("Sign In", systemImage: "person.crop.circle.badge.plus")
+                }
+            } else {
+                Menu {
+                    Button {
+                        isPresenting = true
+                    } label: {
+                        Label("Switch Account", systemImage: "arrow.left.arrow.right")
+                    }
+
+                    Link(destination: AniList.authorizationURL) {
+                        Label("Add Account", systemImage: "plus.circle")
+                    }
+
+                    Button {
+                        currentUser.removeUser(at: 0)
+                    } label: {
+                        Label("Sign Out", systemImage: "minus.circle")
+                    }
+                } label: {
+                    Text("Signed in as ") + Text(currentUser.users[0].name).bold()
+                }.actionSheet(isPresented: $isPresenting) {
+                    ActionSheet(
+                        title: Text("Accounts"),
+                        message: nil, // TODO: Add a descriptive message
+                        buttons: currentUser.users.enumerated().map { index, user in
+                            .default(Text(user.name)) {
+                                currentUser.switchUser(at: index)
+                            }
+                        } + [.cancel()]
+                    )
+                }
+            }
         }.listStyle(SidebarListStyle())
         .navigationTitle("Amincapp")
-        .currentUser()
     }
 }
 
