47c071748fd97514f7263e293701a3018efd2335
LiteLT <kyleerhabor@gmail.com>
2021-01-21 21:18:40 -0500

    Create Int+Abbreviation and Array+Hashable extensions

---
+++ Shared/Extensions/Array+Hashable.swift
@@ -0,0 +1,33 @@
+//
+//  Array+Unique.swift
+//  Amincapp (iOS)
+//
+//  Created by Kyle Erhabor on 1/19/21.
+//
+
+extension Array where Element: Hashable {
+    /// Returns a new array containing unique elements part from the original array.
+    ///
+    /// If an element already appears in the array, only the first element will be returned. This method preserves the order of the array. In this example, wrapping
+    /// an `Array` in a `Set` is a simple and quick way to filter duplicate elements, but does not preserve the order of the array.
+    ///
+    /// ```
+    /// let letters = ["a", "b", "c", "a", "b", "c", "a", "b", "c", "d", "e", "f"]
+    /// print(Set(letters))
+    /// ```
+    ///
+    /// In the next example, the `dinstinct` method preserves the order of the array.
+    ///
+    /// ```
+    /// let letters = ["a", "b", "c", "a", "b", "c", "a", "b", "c", "d", "e", "f"]
+    /// print(letters.distinct())
+    /// ```
+    ///
+    /// - Complexity: O(*n*), where *n* is the length of the array.
+    func distinct() -> [Element] {
+        // Source: https://stackoverflow.com/a/46354989/14695788
+        var seen: Set<Element> = []
+
+        return filter { seen.insert($0).inserted }
+    }
+}
--- Shared/Extensions/Int+Abbreviation.swift
+++ Shared/Extensions/Int+Abbreviation.swift
@@ -8,6 +8,10 @@
 import Foundation
 
 extension Int {
+    /// Returns an abbreviated string of the number.
+    ///
+    /// If the number is less than 1,000, the number is not abbreviated and returned as-is. The units supported are K, M, G, T, P, and E. Anything higher may
+    /// result in inaccurate abbreviation.
     var abbreviated: String {
         if self < 1000 {
             return "\(self)"
@@ -20,6 +24,7 @@ extension Int {
         let exponent = Int(log10(num) / 3)
         let rounded = round(10 * num / pow(1000, Double(exponent))) / 10
 
-        return "\(sign)\(rounded)\(units[exponent - 1])"
+        // Anything higher than six may result in an out of bounds fatal error.
+        return "\(sign)\(rounded)\(units[Swift.min(exponent, 6) - 1])"
     }
 }
