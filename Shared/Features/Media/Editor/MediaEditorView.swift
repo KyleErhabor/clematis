746260e2797d2290772a131cdd046361f600ea92
LiteLT <kyleerhabor@gmail.com>
2021-02-15 17:27:05 -0500

    Reset MediaEditor views
    This "reset" removes all the previous views and starts fresh from scratch. There have been many design changes since the last time these views had been written:
      - The `CurrentUser` class has been replaced with `CurrentUserStore`.
      - The `Json` type alias used to only support dictionaries. It's now become an enum for storing arrays or dictionaries, which has a drastic affect when it comes to mutating the value.
      - View models now rely on the `GraphQLViewModel` class to implement default methods for supporting combine with cancellable requests and a unified declarative interface (on the view model side). This involves rewriting any API calls in the past.
      - Following the MVVM design approach closer
      - more

--- Shared/Features/Media/Editor/MediaEditorView.swift
+++ Shared/Features/Media/Editor/MediaEditorView.swift
@@ -1,138 +1,22 @@
 //
 //  MediaEditorView.swift
-//  Amincapp
+//  Amincapp (iOS)
 //
-//  Created by Kyle Erhabor on 12/10/20.
+//  Created by Kyle Erhabor on 2/15/21.
 //
 
-import SDWebImageSwiftUI
 import SwiftUI
 
 struct MediaEditorView: View {
-    @Environment(\.presentationMode) private var presentationMode
-    @EnvironmentObject private var currentUser: CurrentUser
     @StateObject var viewModel: MediaEditorViewModel
 
     var body: some View {
-        VStack {
-            if viewModel.media != nil {
-                NavigationView {
-                    MediaEditorFormView()
-                        .navigationTitle("List Editor")
-                        .toolbar {
-                            ToolbarItem(placement: .navigationBarLeading) {
-                                Button("Dismiss") {
-                                    presentationMode.wrappedValue.dismiss()
-                                }
-                            }
-
-                            ToolbarItem(placement: .navigationBarTrailing) {
-                                Button("Save") {
-                                    viewModel.saveEntry()
-                                    presentationMode.wrappedValue.dismiss()
-                                }
-                            }
-                        }
-                }
-            } else {
-                NavigationView {
-                    ProgressView().toolbar {
-                        ToolbarItem(placement: .navigationBarLeading) {
-                            Button("Dismiss") {
-                                presentationMode.wrappedValue.dismiss()
-                            }
-                        }
-                    }
-                }
-            }
-        }.environmentObject(viewModel)
-        .onAppear {
-            viewModel.fetchMedia()
-        }
-    }
-}
-
-fileprivate struct MediaEditorFormView: View {
-    @EnvironmentObject private var viewModel: MediaEditorViewModel
-    @EnvironmentObject private var currentUser: CurrentUser
-
-    private let dateFormatter: RelativeDateTimeFormatter = {
-        let formatter = RelativeDateTimeFormatter()
-        formatter.unitsStyle = .full
-
-        return formatter
-    }()
-
-    var body: some View {
-        Form {
-            let media = viewModel.media!
-            let listOptions = currentUser.users.first?.mediaListOptions
-
-            Section(header: Text("Overview")) {
-                MediaEditorFormOverviewView()
-            }.animation(.default)
-
-            Section(header: Text("Progress")) {
-                MediaEditorFormProgressView()
-            }.animation(.default)
-
-//            if let scoreFormat = listOptions?.scoreFormat {
-//                if scoreFormat == .point_10Decimal || scoreFormat == .point_100 {
-//                    let animeList = listOptions?.animeList!
-//
-//                    if media.type == .anime && animeList?.advancedScoringEnabled == true
-//                        && !animeList.advancedScoring!.isEmpty {
-//                        Section(header: Text("Advanced Scores")) {
-//                            MediaEditorFormAdvancedScoresView()
-//                        }.animation(.default)
-//                    }
-//
-//                    let mangaList = listOptions?.mangaList!
-//
-//                    if media.type == .manga && mangaList?.advancedScoringEnabled == true
-//                        && !mangaList?.advancedScoring!.isEmpty {
-//                        Section(header: Text("Advanced Scores")) {
-//                            MediaEditorFormAdvancedScoresView()
-//                        }.animation(.default)
-//                    }
-//                }
-//            }
-//
-//            // Using `else if` instead of a seprate `if` causes the Swift compiler to choke (type-checking in
-//            // reasonable time error).
-//            if media.type == .anime && !listOptions?.animeList!.customLists!.isEmpty {
-//                Section(header: Text("Custom Lists")) {
-//                    MediaEditorFormCustomListView()
-//                }.animation(.default)
-//            }
-//
-//            if media.type == .manga && !listOptions?.mangaList!.customLists!.isEmpty {
-//                Section(header: Text("Custom Lists")) {
-//                    MediaEditorFormCustomListView()
-//                }.animation(.default)
-//            }
-
-            Section(header: Text("Notes")) {
-                MediaEditorFormNotesView()
-            }.animation(.default)
-
-            Section(header: Text("List Settings"), footer: HStack {
-                if let lastUpdate = media.mediaListEntry?.updatedAt {
-                    let lastUpdateDate = Date(timeIntervalSince1970: TimeInterval(lastUpdate))
-                    let relativeTime = dateFormatter.localizedString(for: lastUpdateDate, relativeTo: Date())
-
-                    Spacer()
-                    Text("Last Updated ").bold() + Text(relativeTime)
-                }
-            }.padding()) {
-                MediaEditorFormListSettingsView()
-            }.animation(.default)
-        }
+        Text("Hello, World!")
     }
 }
 
 struct MediaEditorView_Previews: PreviewProvider {
     static var previews: some View {
-        MediaEditorView(viewModel: MediaEditorViewModel(id: 30009))
+        MediaEditorView(viewModel: .init(id: 302))
     }
 }
--- Shared/Features/Media/Editor/MediaEditorViewModel.swift
+++ Shared/Features/Media/Editor/MediaEditorViewModel.swift
@@ -1,115 +1,16 @@
 //
 //  MediaEditorViewModel.swift
-//  Amincapp
+//  Amincapp (iOS)
 //
-//  Created by Kyle Erhabor on 12/11/20.
+//  Created by Kyle Erhabor on 2/15/21.
 //
 
 import Foundation
 
-class MediaEditorViewModel: ObservableObject {
-    @Published var media: MediaEditorQuery.Data.Medium?
-
-    private let id: Int
+class MediaEditorViewModel: GraphQLViewModel, ObservableObject {
+    let id: Int
 
     init(id: Int) {
         self.id = id
-    }
-
-    func fetchMedia() {
-        GraphQLNetwork.shared.fetch(query: MediaEditorQuery(id: id)) { result in
-            switch result {
-                case .success(let query):
-                    if let media = query.data?.media {
-                        self.media = media
-                    }
-
-                    if let errors = query.errors {
-                        logger.error("\(errors)")
-                    }
-                case .failure(let err):
-                    logger.error("\(err)")
-            }
-        }
-    }
-
-    func saveEntry() {
-//        let customLists = media?.mediaListEntry?.customLists == nil
-//            ? []
-//            : Array(media!.mediaListEntry!.customLists!.keys)
-//
-//        let advancedScores = media?.mediaListEntry?.advancedScores == nil
-//            ? []
-//            : Array(media!.mediaListEntry!.advancedScores!.values) as! [Double]
-//
-//        let startedAt = FuzzyDateInput(
-//            year: media?.mediaListEntry?.startedAt?.year,
-//            month: media?.mediaListEntry?.startedAt?.month,
-//            day: media?.mediaListEntry?.startedAt?.day
-//        )
-//
-//        let completedAt = FuzzyDateInput(
-//            year: media?.mediaListEntry?.completedAt?.year,
-//            month: media?.mediaListEntry?.completedAt?.month,
-//            day: media?.mediaListEntry?.completedAt?.day
-//        )
-//
-//        if let id = media?.mediaListEntry?.id, id != -1 {
-//            GraphQLNetwork.shared.perform(mutation: UpdateMediaListEntryMutation(
-//                id: id,
-//                status: media?.mediaListEntry?.status,
-//                score: media?.mediaListEntry?.score,
-//                progress: media?.mediaListEntry?.progress,
-//                progressVolumes: media?.mediaListEntry?.progressVolumes,
-//                repeat: media?.mediaListEntry?.repeat,
-//                priority: media?.mediaListEntry?.priority,
-//                private: media?.mediaListEntry?.private,
-//                notes: media?.mediaListEntry?.notes,
-//                hiddenFromStatusLists: media?.mediaListEntry?.hiddenFromStatusLists,
-//                customLists: customLists,
-//                advancedScores: advancedScores,
-//                startedAt: startedAt,
-//                completedAt: completedAt
-//            )) { result in
-//                switch result {
-//                    case .success(let query):
-//                        if let extensions = query.extensions {
-//                            logger.info("\(extensions)")
-//                        }
-//
-//                        if let errors = query.errors {
-//                            logger.error("\(errors)")
-//                        }
-//                    case .failure(let err):
-//                        logger.error("\(err)")
-//                }
-//            }
-//        } else {
-//            GraphQLNetwork.shared.perform(mutation: CreateMediaListEntryMutation(
-//                mediaId: id,
-//                status: media?.mediaListEntry?.status ?? .planning,
-//                score: media?.mediaListEntry?.score ?? 0,
-//                progress: media?.mediaListEntry?.progress ?? 0,
-//                progressVolumes: media?.mediaListEntry?.progressVolumes ?? 0,
-//                repeat: media?.mediaListEntry?.repeat ?? 0,
-//                priority: media?.mediaListEntry?.priority ?? 0,
-//                private: media?.mediaListEntry?.private ?? false,
-//                notes: media?.mediaListEntry?.notes ?? "",
-//                hiddenFromStatusLists: media?.mediaListEntry?.hiddenFromStatusLists ?? false,
-//                customLists: customLists,
-//                advancedScores: advancedScores,
-//                startedAt: startedAt,
-//                completedAt: completedAt
-//            )) { result in
-//                switch result {
-//                    case .success(let query):
-//                        if let errors = query.errors {
-//                            logger.error("\(errors)")
-//                        }
-//                    case .failure(let err):
-//                        logger.error("\(err)")
-//                }
-//            }
-//        }
     }
 }
